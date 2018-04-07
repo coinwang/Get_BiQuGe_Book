809e7f27795e901831e2a4600665c46abe5f43c9
Kunner <493166951@qq.com>
2018-04-07 22:13:16 +0800

    GetBook.py
    1. 更换爬取网站；2. 去除书名搜索；3. 增加对空的章节页面的异常处理；

--- GetBook.py
+++ GetBook.py
@@ -18,45 +18,48 @@ def GetHtml(htmlUrl):
         print('\n\tWARN\t%s' %e)
         return htmlUrl
 
-# 按书名搜索笔趣阁书库
-def SearchBook(bookName):
-    searchUrl = 'http://www.biquge5200.com/modules/article/search.php?searchkey=' +bookName
-    searchHtml = GetHtml(searchUrl)
-
-    soup = BeautifulSoup(searchHtml, 'html.parser')
-    searchBookNameHtmlList = soup.select('.odd')
-    if len(searchBookNameHtmlList) == 0:
-        return None
-
-    patternBookInfo = r'href'
-    patternBookName = r'/">(.*?)</a>'
-    patternBookUrl = r'http(.*)"'
-
-    for searchBookNameHtml in searchBookNameHtmlList:
-        searchBookNameHtml = str(searchBookNameHtml)
-        getBookInfo = re.search(patternBookInfo, searchBookNameHtml)
-        if getBookInfo is not None:
-            getBookName = re.search(patternBookName, searchBookNameHtml).group().replace('/">', '').replace('</a>', '')
-            if getBookName == bookName:
-                bookUrl = re.search(patternBookUrl, searchBookNameHtml).group().replace('"', '')
-                return bookUrl
+# # 按书名搜索笔趣阁书库
+# def SearchBook(bookName):
+#     searchUrl = 'http://www.biquge5200.com/modules/article/search.php?searchkey=' +bookName
+#     searchHtml = GetHtml(searchUrl)
+#
+#     soup = BeautifulSoup(searchHtml, 'html.parser')
+#     searchBookNameHtmlList = soup.select('.odd')
+#     if len(searchBookNameHtmlList) == 0:
+#         return None
+#
+#     patternBookInfo = r'href'
+#     patternBookName = r'/">(.*?)</a>'
+#     patternBookUrl = r'http(.*)"'
+#
+#     for searchBookNameHtml in searchBookNameHtmlList:
+#         searchBookNameHtml = str(searchBookNameHtml)
+#         getBookInfo = re.search(patternBookInfo, searchBookNameHtml)
+#         if getBookInfo is not None:
+#             getBookName = re.search(patternBookName, searchBookNameHtml).group().replace('/">', '').replace('</a>', '')
+#             if getBookName == bookName:
+#                 bookUrl = re.search(patternBookUrl, searchBookNameHtml).group().replace('"', '')
+#                 return bookUrl
 
 
 # 获取小说目录
 def GetChapterHtml(htmlSource, bookUrl):
     soup = BeautifulSoup(htmlSource, 'html.parser')
+    # 小说书名
     bookName = soup.select('h1')[0]
     bookName = str(bookName).replace('<h1>', '').replace('</h1>', '')
+    # print(bookName)
 
-    chapterNumList = []
-    for line in soup.body.find_all('dd'):
-        pattern = bookUrl +r'(.*?)\.html'
+    chapterNumList = []  # 章节url编号列表
+
+    for line in soup.body.find_all('dd'):  # 网页中章节列表
+        pattern = r'href="/(.*?)/(\d*?)\.html"'
         List = str(line)
-        chapterNumList.append(re.findall(pattern, List)[0])
-
-    chapterNumList = set(chapterNumList)
-    chapterNumList = list(chapterNumList)
-    chapterNumList.sort()
+        # print(List)
+        chapterNum = re.findall(pattern, List)[0]  # 章节编号
+        # print(chapterNum)
+        chapterNumList.append(chapterNum[1])
+    # print(chapterNumList)
 
     chapterHtml = []
     for chapterNum in chapterNumList:
@@ -68,52 +71,72 @@ def GetArticle(articleHtmlSource, txtFile):
     soup = BeautifulSoup(articleHtmlSource, 'html.parser')
 
     # 解析小数章节标题
-    articleTitle = soup.select('h1')[0]
-    articleTitle = str(articleTitle).replace('<h1>', '').replace('</h1>', '\n')
-    print(articleTitle)
-    txtFile.write(articleTitle)
+    try:
+        articleTitle = soup.select('h1')[0]
+        articleTitle = str(articleTitle).replace('<h1>', '').replace('</h1>', '\n')
+        print(articleTitle)
+        txtFile.write('\n'+ articleTitle)
+    except Exception as e:
+        print(e)
+
     txtFile.flush()
 
     # 解析小说章节正文
-    articleBody = soup.select('#content')[0]
-    txtFile.write(str(articleBody).replace('<div id="content">', '').replace('</div>', '').replace('<br/>','\n'))
+    try:
+        articleBody = soup.select('#content')[0]
+        # print(articleBody)
+        articleLine = str(articleBody).replace('<div id="content">', '').replace('</div>', '').replace('<br/>','')
+        txtFile.write(articleLine)
+    except Exception as e:
+        print(e)
+
     txtFile.flush()
 
 
 if __name__ == '__main__':
+    # BookName = input('Please input a book Name: ')
+    BookName = '111'
 
-    BookName = input('Please input a book Name: ')
-
-    Speed = 0.1
+    Speed = 0
     SleepTime = 2
 
-    print('\nStart search book...')
-    SaveRoute = './'
-    BookUrl = SearchBook(BookName)
+    # print('\nStart search book...')
+    SaveRoute = './'        # 下载保存位置
+    # BookUrl = SearchBook(BookName)
+    # BookUrl = 'http://www.biqu.cm/2_2077/'
+    BookUrl = 'http://www.biquge.com.tw/3_3593/'
     if BookUrl == None:
         print('\tERROR\tNot Find %s' %BookName)
         sys.exit(1)
 
     print('\nStart download book...')
     print('\nThe book will save at ' +SaveRoute + BookName +'.txt\n')
+    # 书籍目录页
     HtmlSource = GetHtml(BookUrl)
-    ChapterUrl = GetChapterHtml(HtmlSource, BookUrl)
+    # print(HtmlSource)
+    ChapterUrl = GetChapterHtml(HtmlSource, BookUrl)   # 章节地址列表
+    # print(ChapterUrl)
     TxtFile = open(SaveRoute+ BookName +'.txt', 'w', encoding='utf-8')
 
-    TotleNum = len(ChapterUrl)
-    FinishNum = 1
+    TotleNum = len(ChapterUrl)    # 要下载的章节总数
+    FinishNum = 1                 # 已下载的章节数
+
+    # # 单章节源码分析
+    # ArticleHtmlSource = GetHtml('http://www.biqu.cm/2_2077/3757754.html')
+    # GetArticle(ArticleHtmlSource, TxtFile)
 
     for i in ChapterUrl:
         print("%s/%s" %(FinishNum, TotleNum), end='\t')         # 显示爬取进度
         FinishNum += 1
 
         ArticleHtmlSource = GetHtml(i)
-        while ArticleHtmlSource.startswith('http'):
+        while ArticleHtmlSource.startswith('http'):       # 异常情况延时下载
             print('\tWARN\tTry Later')
             time.sleep(SleepTime)
             ArticleHtmlSource = GetHtml(i)
 
         GetArticle(ArticleHtmlSource, TxtFile)
+        # 每章节下载间歇
         time.sleep(Speed)
 
     TxtFile.close()
